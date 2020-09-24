5b47479cb9d0ed8bdc1a28a7ca49e561c69e23e8
sunhwa-kim <ksh0810@gmail.com>
2020-09-25 02:34:20 +0900

    es5,es6

--- HelloWorld/hello.js
+++ HelloWorld/hello.js
@@ -29,7 +29,52 @@ console.log(f, typeof f);
 const g = Symbol();
 const h = Symbol(37);
 const m = Symbol('test');
-const n = Symbol('test');
+const o = Symbol('test');
 
 console.log(g, typeof g);
-console.log(m===n);
+console.log(m===o);
+
+
+if(true) console.log('항상 실행');
+if(false) console.log('절대 실행 되지 않음');
+
+if({}) console.log('{} Truthy',{});
+if(-1) console.log('-1 Truthy' , -1);
+
+
+const n = 15;
+const multipleOfThree = n%3 ===0;
+const multipleOfFive = n%5 ===0;
+
+if(multipleOfThree && multipleOfFive ){
+    console.log('n 은 15의 배수');
+} else if (multipleOfThree){
+    console.log('n 은 3의 배수');
+} else if (multipleOfFive ){
+    console.log('n 은 5의 배수');
+} else{
+    console.log('n은 3의 배수도 아니고, 5의 배수도 아니다.');
+}
+
+// 중첩 이용
+if(multipleOfThree && multipleOfFive ){
+    console.log('n 은 15의 배수');
+} else {
+    if (multipleOfThree){
+        console.log('n 은 3의 배수');
+    } else if (multipleOfFive ){
+        console.log('n 은 5의 배수');
+    } else{
+        console.log('n은 3의 배수도 아니고, 5의 배수도 아니다.');
+    }
+}
+
+// &&  순서 : 앞 표현식 참 일때만, 뒤 표현식 평가 필요 생겨서 뒤의 표현식 실행
+let nn = 5;
+(nn%5===0) && console.log('and 연산자 , 앞 이 참이기 때문에  뒤 실행 ');
+
+// || : 둘 중에 하나만 참이면 참 => 앞 표현식 참이면 뒤 표현식 평가할 필요 없어서 실행하지 않는다.
+nn = 5
+n%5 ===0 || console.log('or연산자 앞이 참이면 그냥 실행');
+
+n%6 === 0 || console.log('or연산자 앞 false 시 실행');
